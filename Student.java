12ff87531a7636344762114d5ac9b62730b16b0d
PRABHAS77-ALT <nagaprabhassake@gmail.com>
2025-09-19 10:30:12 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class Student {
+    private int studentId;
+    private String studentName;
+
+    public Student(int studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    // Getters for student properties
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student ID: " + studentId + ", Name: " + studentName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId);
+    }
+}
